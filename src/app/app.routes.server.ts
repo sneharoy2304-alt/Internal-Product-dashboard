53f6dc3b5dd3e3d46ef3a73fec8f71b03971d570
lakshyadeepgogoi <lakshyadeep19@gmail.com>
2026-07-01 11:33:48 +0530

    feat: add client-side routes for detail and edit pages and fix CSS nesting syntax

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'detail/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'edit/:id',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
